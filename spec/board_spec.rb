6f966a4d8f45eeaafca3860be6e537748be362aa
kiptubei <kiptubei@gmail.com>
2020-07-09 13:48:10 +0300

    Added rspec for invalid moves

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,55 +1,69 @@
-#spec/board_spec.rb
+# spec/board_spec.rb
 
 require './lib/board'
 
 describe Board do
-  subject { Board.new(order)}
+  subject { Board.new(order) }
 
-  context "#win_combinations?" do
+  context '#win_combinations?' do
     let(:order) { 3 }
-    
-    it "returns true if there is a row win combination" do
+
+    it 'returns true if there is a row win combination' do
       place = subject.move_empty?(1)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
       place = subject.move_empty?(2)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
       place = subject.move_empty?(3)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
 
-      expect(subject.row_check?("X")).to eql(true)
+      expect(subject.row_check?('X')).to eql(true)
     end
 
-    it "returns true if there is a column win combination" do
+    it 'returns true if there is a column win combination' do
       place = subject.move_empty?(1)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
       place = subject.move_empty?(4)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
       place = subject.move_empty?(7)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
 
-      expect(subject.column_check?("X")).to eql(true)
+      expect(subject.column_check?('X')).to eql(true)
     end
 
-    it "returns true if there is a diagonal win combination: diagonal I" do
+    it 'returns true if there is a diagonal win combination: diagonal I' do
       place = subject.move_empty?(1)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
       place = subject.move_empty?(5)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
       place = subject.move_empty?(9)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
 
-      expect(subject.diagonal_check?("X")).to eql(true)
+      expect(subject.diagonal_check?('X')).to eql(true)
     end
 
-    it "returns true if there is a diagonal win combination: diagonal II" do
+    it 'returns true if there is a diagonal win combination: diagonal II' do
       place = subject.move_empty?(3)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
       place = subject.move_empty?(5)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
       place = subject.move_empty?(7)
-      subject.place_move(place,"X")
+      subject.place_move(place, 'X')
 
-      expect(subject.diagonal_check?("X")).to eql(true)
+      expect(subject.diagonal_check?('X')).to eql(true)
+    end
+  end
+
+  context '#move_empty?' do
+    let(:order) { 3 }
+
+    it 'returns array if valid move is made' do
+      expect(subject.move_empty?(1).class).to eql(Array)
+    end
+
+    it 'returns Original Integer value if invalid move is made' do
+      place = subject.move_empty?(1)
+      subject.place_move(place, 'X')
+      expect(subject.move_empty?(1)).to eql(1)
     end
   end
 end
