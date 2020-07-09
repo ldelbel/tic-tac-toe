45534bc8e2dd8d2526308a57984abe56cb63b74a
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-07-09 13:10:55 +0300

    - Initialized rspec for testing - Added tests for board class - Added row win test - Added column win test - Added diagonal win test

---
+++ spec/board_spec.rb
@@ -0,0 +1,44 @@
+#spec/board_spec.rb
+
+require './lib/board'
+
+describe Board do
+  subject { Board.new(order)}
+
+  context "#row_check?" do
+    let(:order) { 3 }
+    
+    it "returns true if there is a row win combination" do
+      place = subject.move_empty?(1)
+      subject.place_move(place,"X")
+      place = subject.move_empty?(2)
+      subject.place_move(place,"X")
+      place = subject.move_empty?(3)
+      subject.place_move(place,"X")
+
+      expect(subject.row_check?("X")).to eql(true)
+    end
+
+    it "returns true if there is a column win combination" do
+      place = subject.move_empty?(1)
+      subject.place_move(place,"X")
+      place = subject.move_empty?(4)
+      subject.place_move(place,"X")
+      place = subject.move_empty?(7)
+      subject.place_move(place,"X")
+
+      expect(subject.column_check?("X")).to eql(true)
+    end
+
+    it "returns true if there is a diagonal win combination" do
+      place = subject.move_empty?(1)
+      subject.place_move(place,"X")
+      place = subject.move_empty?(5)
+      subject.place_move(place,"X")
+      place = subject.move_empty?(9)
+      subject.place_move(place,"X")
+
+      expect(subject.diagonal_check?("X")).to eql(true)
+    end
+  end
+end
