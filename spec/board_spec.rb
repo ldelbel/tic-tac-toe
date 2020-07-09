e486d5a20ce63e54203f811e804c6c5f5f1e189e
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-07-09 13:17:48 +0300

    - Added test for diagonal II and changed the context title to win_combinations

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,7 +5,7 @@ require './lib/board'
 describe Board do
   subject { Board.new(order)}
 
-  context "#row_check?" do
+  context "#win_combinations?" do
     let(:order) { 3 }
     
     it "returns true if there is a row win combination" do
@@ -30,12 +30,23 @@ describe Board do
       expect(subject.column_check?("X")).to eql(true)
     end
 
-    it "returns true if there is a diagonal win combination" do
+    it "returns true if there is a diagonal win combination: diagonal I" do
       place = subject.move_empty?(1)
       subject.place_move(place,"X")
       place = subject.move_empty?(5)
       subject.place_move(place,"X")
       place = subject.move_empty?(9)
+      subject.place_move(place,"X")
+
+      expect(subject.diagonal_check?("X")).to eql(true)
+    end
+
+    it "returns true if there is a diagonal win combination: diagonal II" do
+      place = subject.move_empty?(3)
+      subject.place_move(place,"X")
+      place = subject.move_empty?(5)
+      subject.place_move(place,"X")
+      place = subject.move_empty?(7)
       subject.place_move(place,"X")
 
       expect(subject.diagonal_check?("X")).to eql(true)
